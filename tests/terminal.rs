ed794f359f004933fa00b511ac4064a2484f14ae
Sufilevy <sufilevy@gmail.com>
2023-05-02 20:10:16 +0300

    feat(benches): add benches for terminal

--- tests/terminal.rs
+++ tests/terminal.rs
@@ -1,10 +1,14 @@
+#![feature(test)]
+extern crate test;
+
+use std::error::Error;
+
 use ratatui::{
     backend::{Backend, TestBackend},
     layout::Rect,
-    widgets::Paragraph,
     Terminal,
+    widgets::Paragraph,
 };
-use std::error::Error;
 
 #[test]
 fn terminal_buffer_size_should_be_limited() {
@@ -33,4 +37,45 @@ fn terminal_draw_returns_the_completed_frame() -> Result<(), Box<dyn Error>> {
     assert_eq!(frame.buffer.get(0, 0).symbol, "t");
     assert_eq!(frame.area, Rect::new(0, 0, 8, 8));
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use test::Bencher;
+
+    use ratatui::backend::CrosstermBackend;
+    use ratatui::backend::TermionBackend;
+
+    use super::*;
+
+    #[bench]
+    fn crossterm_create_terminal(b: &mut Bencher) {
+        let stdout = std::io::stdout();
+        b.iter(|| {
+            let backend = CrosstermBackend::new(&stdout);
+            let _ = Terminal::new(backend);
+        });
+    }
+
+    #[cfg(feature = "termion")]
+    #[bench]
+    fn termion_create_terminal(b: &mut Bencher) {
+        let stdout = std::io::stdout();
+        b.iter(|| {
+            let backend = TermionBackend::new(&stdout);
+            let _ = Terminal::new(backend);
+        });
+    }
+
+    #[bench]
+    fn terminal_draw(b: &mut Bencher) {
+        let backend = TestBackend::new(10, 10);
+        let mut terminal = Terminal::new(backend).unwrap();
+        b.iter(|| {
+            terminal.draw(|f| {
+                let paragraph = Paragraph::new("Test");
+                f.render_widget(paragraph, f.size());
+            }).unwrap();
+        });
+    }
 }
